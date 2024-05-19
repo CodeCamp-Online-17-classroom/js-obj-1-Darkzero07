72e98078c5ec1928e8257eb9f017b28a64078d9a
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 21:08:19 +0700

    js lab 47

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let person = {
+  name: "john",
+  age: "40",
+  gender: "male",
+  address: "BKK",
+  telephone: "0123456789",
+};
+
+console.log(person)
+console.log(typeof(person))
